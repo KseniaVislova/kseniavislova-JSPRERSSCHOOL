0038ae8c7d69be080f7744108267c5b21d09a058
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-01-11 20:00:52 +0300

    feat: add styles for adaptive header (it has mistake with menu-hamburger)

--- portfolio/index.js
+++ portfolio/index.js
@@ -5,13 +5,23 @@ let menuBurger = document.querySelector('.menu-burger');
 let close = document.querySelector('.close');
 let navigation = document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav-link');
+let modal = document.querySelector('.modal');
 
 const closeMenu = (event) => {
   if (event.target.classList.contains('nav-link')) {
     navigation.classList.remove('open');
+    modal.classList.remove('modal-open');
   }
 }
 
-menuBurger.addEventListener('click', () => navigation.classList.toggle('open'));
-close.addEventListener('click', () => navigation.classList.toggle('open'));
+menuBurger.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  modal.classList.toggle('modal-open');
+});
+
+close.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  modal.classList.toggle('modal-open');
+});
+
 navigation.addEventListener('click', closeMenu);
