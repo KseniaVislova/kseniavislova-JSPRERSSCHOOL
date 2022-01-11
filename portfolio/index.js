4def6217056c3834094adf4b5053e085cc61fee2
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-01-11 15:48:40 +0300

    feat: add menu-hamburger for 768px

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,2 +1,17 @@
 let score = 'Вёрстка валидная: 10 \nВёрстка семантическая: 20 \nВёрстка соответствует макету: 48 \nТребования к css: 12 \nИнтерактивность, реализуемая через css: 20';
 console.log(score);
+
+let menuBurger = document.querySelector('.menu-burger');
+let close = document.querySelector('.close');
+let navigation = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav-link');
+
+const closeMenu = (event) => {
+  if (event.target.classList.contains('nav-link')) {
+    navigation.classList.remove('open');
+  }
+}
+
+menuBurger.addEventListener('click', () => navigation.classList.toggle('open'));
+close.addEventListener('click', () => navigation.classList.toggle('open'));
+navigation.addEventListener('click', closeMenu);
