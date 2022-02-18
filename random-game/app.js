0febf7b4357cb36a8c32e714e40a11e5fd6d10a1
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-02-18 22:07:14 +0300

    feat: add func for rendering random numbers

--- random-game/app.js
+++ random-game/app.js
@@ -2,11 +2,23 @@ const board = document.querySelector('.board');
 let squares = [];
 const width = 4;
 
-const createBoard = () => {
-  for (let i = 0; i <= width * width; i++) {
-    const item = `<div>0</div>`;
-    board.insertAdjacentHTML('beforeend', item);
+const startNumbers = () => {
+  let random = Math.floor(Math.random() * squares.length);
+  if (squares[random].innerHTML == 0) {
+    squares[random].innerHTML = 2;
+  } else {
+    startNumbers()
   }
 }
 
-createBoard()
+const createBoard = () => {
+  for (let i = 0; i <= width * width; i++) {
+    const item = `<div class="square">0</div>`;
+    board.insertAdjacentHTML('beforeend', item);
+  }
+  squares = document.querySelectorAll('.square');
+  startNumbers();
+  startNumbers();
+}
+
+createBoard();
